b520acbbfcebf4bf7aba59f06ac65e0bffa07593
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-05-15 22:21:29 +0300

    Add autolayout and dice functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceLeft.image = diceArray[Int.random(in: 0...5)]
+        diceRight.image = diceArray[Int.random(in: 0...5)]
     }
 
-
+    @IBOutlet weak var diceLeft: UIImageView!
+    @IBOutlet weak var diceRight: UIImageView!
+    
+    var diceArray = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix") ]
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        diceLeft.image = diceArray[Int.random(in: 0...5)]
+        diceRight.image = diceArray[Int.random(in: 0...5)]
+    }
+    
 }
 
